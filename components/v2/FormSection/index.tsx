384b71200aaab5220fda42fcf859437b9188279e
rogeritael <rogeritael.contato@gmail.com>
2023-12-02 20:07:00 -0300

    reponsivo do formulário de orçamento

--- components/v2/FormSection/index.tsx
+++ components/v2/FormSection/index.tsx
@@ -1,10 +1,17 @@
 import './styles.scss'
 import { Form } from '../Form'
+import mask from '@/assets/v2/mask.png'
+import bg from '@/assets/v2/form_bg.png'
+import Image from 'next/image'
 
 export function FormSection(){
     return (
         <section id="form_section">
             <Form />
+            <figure className="background">
+                <Image src={bg} alt='imagem de fundo' />
+                <Image src={mask} alt='máscara da imagem de fundo' />
+            </figure>
         </section>
     )
 }
