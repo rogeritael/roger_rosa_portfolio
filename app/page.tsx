acbe21b09057dad4dc3db7941e6d70baaa6d293a
rogeritael <rogeritael.contato@gmail.com>
2023-12-06 11:28:51 -0300

    criando a página inicial do site

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import styles from './page.module.css'
+import Image from 'next/image'
+import './page.scss'
+import logo from '@/assets/v2/rounded_text.png'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      portfólio
+    <main id='index'>
+      <Image src={logo} className='logo' alt='logo'/>
     </main>
   )
 }
