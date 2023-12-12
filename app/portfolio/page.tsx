e38849298061057d696fb69566ca59c1f08d7b0a
rogeritael <rogeritael.contato@gmail.com>
2023-12-12 08:32:48 -0300

    header do portfolio

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+import { Header } from "@/components/v1/Header";
+import { useState } from "react";
+
 export default function Portfolio(){
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
     return(
         <main id="portfolio">
-            
+            <Header setIsMobileMenuOpen={setIsMobileMenuOpen} />
         </main>
     )
 }
