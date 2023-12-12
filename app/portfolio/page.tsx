50fc644b44e35ea710a83ed50f0b59d3a8448efd
rogeritael <rogeritael.contato@gmail.com>
2023-12-12 08:45:34 -0300

    cover do portfólio

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,12 +1,30 @@
 'use client'
 import { Header } from "@/components/v1/Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import { Cover } from "@/components/v1/Cover";
 
 export default function Portfolio(){
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+
+    useEffect(() => {
+        Aos.init({
+            duration: 1500
+        })
+    },[])
+
     return(
         <main id="portfolio">
+            {/* <MobileMenu isMobileMenuOpen={isMobileMenuOpen} setIsMobileMenuOpen={setIsMobileMenuOpen} /> */}
             <Header setIsMobileMenuOpen={setIsMobileMenuOpen} />
+            <Cover />
+            {/* <Cover /> */}
+            {/* <SkillsContainer /> */}
+            {/* <Projects /> */}
+            {/* <FAQ /> */}
+            {/* <ContactForm /> */}
+            {/* <Footer /> */}
         </main>
     )
 }
